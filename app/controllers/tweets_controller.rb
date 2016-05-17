55ae481307a66a89def959812bf439bfff0fc1af
Ryan Hageman <improv42@gmail.com>
2016-05-16 19:55:19 -0500

    Set up index view and controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
@@ -28,5 +29,15 @@ class TweetsController < ApplicationController
   end
 
   def _forms
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
